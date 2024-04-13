40d454f075655332de1d1edc47242fab9b60dcb5
Bilgehan KAYA <bilgehankaya79@gmail.com>
2024-04-13 17:23:15 +0300

    first commit

---
+++ NetCoreProject/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ProductComponent } from './components/product/product.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { CategoryComponent } from './components/category/category.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule, ProductComponent, NaviComponent, CategoryComponent ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title:string = 'NetCoreProject';
+  user:string = "Bilgehan KAYA";
+}
---
+++ NetCoreProject/src/app/components/product/product.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Product } from '../../models/product';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+  products : Product[] = [];
+  constructor() {}
+  ngOnInit(): void {
+    
+  }
+}
