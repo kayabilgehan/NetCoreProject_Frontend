845eb5a17b0a343fb2c70ac68a19c576969acc93
Bilgehan KAYA <bilgehankaya79@gmail.com>
2024-04-14 03:01:53 +0300

    Product service added.

--- NetCoreProject/src/app/components/product/product.component.ts
+++ NetCoreProject/src/app/components/product/product.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
 import { Product } from '../../models/product';
 
 @Component({
@@ -9,10 +10,17 @@ import { Product } from '../../models/product';
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
-export class ProductComponent {
-  products : Product[] = [];
-  constructor() {}
+export class ProductComponent implements OnInit {
+  products: Product[] = [];
+  dataLoaded: boolean = false;
+  constructor(private productService: ProductService) {}
   ngOnInit(): void {
-    
+    this.getProducts();
+  }
+  getProducts(){
+    this.productService.getProducts().subscribe((response) => {
+      this.products = response.data;
+      this.dataLoaded = true;
+    });
   }
 }
